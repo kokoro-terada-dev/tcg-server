6d9571f48a075674be1a5c6a203bccb16fe4aca3
mary893ren <mary893ren@gmail.com>
2026-06-20 02:29:49 +0900

    add socket welcome

---
+++ src/roomManager.ts
@@ -0,0 +1,36 @@
+const rooms = new Map<
+  string,
+  string[]
+>();
+
+export function createRoom(
+  socketId: string
+) {
+  const roomId = Math.random()
+    .toString(36)
+    .substring(2, 6)
+    .toUpperCase();
+
+  rooms.set(roomId, [socketId]);
+
+  return roomId;
+}
+
+export function joinRoom(
+  roomId: string,
+  socketId: string
+) {
+  const room = rooms.get(roomId);
+
+  if (!room) {
+    return false;
+  }
+
+  if (room.length >= 2) {
+    return false;
+  }
+
+  room.push(socketId);
+
+  return true;
+}
