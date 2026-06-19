96a92f32012f61cf4e3680c4bff27208218d07df
mary893ren <mary893ren@gmail.com>
2026-06-20 02:25:50 +0900

    add socket welcome

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,81 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import {
+    createRoom,
+    joinRoom
+} from "./roomManager";
 
 const app = express();
 
 app.get("/", (_, res) => {
-  res.send("Server Running");
+    res.send("Server Running");
 });
 
 const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
-  cors: {
-    origin: "*",
-  },
+    cors: {
+        origin: "*",
+    },
 });
 
 io.on("connection", (socket) => {
-  console.log("connected:", socket.id);
+    console.log("connected:", socket.id);
 
-  socket.emit("welcome", {
-    socketId: socket.id,
-  });
+    socket.emit("welcome", {
+        socketId: socket.id,
+    });
 
-  socket.on("disconnect", () => {
-    console.log("disconnected:", socket.id);
-  });
+    socket.on("disconnect", () => {
+        console.log("disconnected:", socket.id);
+    });
+
+    socket.on("create-room", () => {
+        const roomId =
+            createRoom(socket.id);
+
+        socket.join(roomId);
+
+        socket.emit(
+            "room-created",
+            roomId
+        );
+    });
+
+    socket.on("disconnect", () => {
+        console.log("disconnected:", socket.id);
+    });
+
+    socket.on(
+        "join-room",
+        (roomId) => {
+            const success =
+                joinRoom(
+                    roomId,
+                    socket.id
+                );
+
+            if (!success) {
+                socket.emit(
+                    "join-failed"
+                );
+
+                return;
+            }
+
+            socket.join(roomId);
+
+            io.to(roomId).emit(
+                "room-joined",
+                roomId
+            );
+        }
+    );
 });
 
 const PORT = process.env.PORT || 3000;
 
 httpServer.listen(PORT, () => {
-  console.log(`server start ${PORT}`);
+    console.log(`server start ${PORT}`);
 });
